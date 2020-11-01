910ba17e8788f355de582b3ead562d54066011b6
matata samuel <mattasamuel3@gmail.com>
2020-11-02 00:53:10 +0300

    added Neighborhood class methods

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -6,6 +6,7 @@ from django_countries.fields import CountryField
 
 
 # Create your models here.
+# Neighborhood class
 class Neighborhood(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
@@ -13,3 +14,47 @@ class Neighborhood(models.Model):
     Admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
     pub_date = models.DateTimeField(auto_now_add=True)
     country = CountryField(blank_label='(select country)', default='NG')
+
+    def save_neighborhood(self):
+        self.save()
+    
+    def delete_neighborhood(self):
+        self.delete()
+        
+    @classmethod
+    def get_neighborhoods(cls):
+        projects = cls.objects.all()
+        return projects
+    
+    @classmethod
+    def search_neighborhoods(cls, search_term):
+        projects = cls.objects.filter(name__icontains=search_term)
+        return projects    
+    
+    @classmethod
+    def get_by_admin(cls, Admin):
+        projects = cls.objects.filter(Admin=Admin)
+        return projects
+    
+    
+    @classmethod
+    def get_neighborhood(request, neighborhood):
+        try:
+            project = Neighborhood.objects.get(pk = id)
+            
+        except ObjectDoesNotExist:
+            raise Http404()
+        
+        return project
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'My Neighborhood'
+        verbose_name_plural = 'Neighborhoods'
+
+
+# profile class
+
