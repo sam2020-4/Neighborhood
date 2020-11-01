ae39ebd0b60f118be566fe2ae165e80f01c40c40
matata samuel <mattasamuel3@gmail.com>
2020-11-01 21:20:16 +0300

    imported app to the setting

--- Nproject/settings.py
+++ Nproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'neighborhood.apps.NeighborhoodConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
