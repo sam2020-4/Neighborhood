0856d89b0c28bee5f80f5410eb0ea2122c73cf05
matata samuel <mattasamuel3@gmail.com>
2020-11-02 00:35:15 +0300

    inserted neighborhood class and imported datetime

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime as dt
+
 
 # Create your models here.
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    occupants_count = models.IntegerField()   
+    Admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
