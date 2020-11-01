341717378712822b79a51ffbb836d5012e3dc101
matata samuel <mattasamuel3@gmail.com>
2020-11-02 02:34:49 +0300

    entered a new business form

--- neighborhood/forms.py
+++ neighborhood/forms.py
@@ -1,2 +1,10 @@
 from django import forms
 from .models import *
+
+class NewBusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['Admin', 'pub_date', 'admin_profile']
+        widgets = {
+          'address': forms.Textarea(attrs={'rows':1, 'cols':10,}),
+        }
