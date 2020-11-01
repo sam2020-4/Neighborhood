afc1e932652f5e2bdaa4cc3c0ba7076c1f43ee1f
matata samuel <mattasamuel3@gmail.com>
2020-11-02 02:08:08 +0300

    added get business by_id

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -58,6 +58,15 @@ def search_businesses(request):
         message = "You haven't searched for any term"
         return render(request, 'search.html', {"message": message})
 
+# get business by id method
+def get_business(request, id):
 
-
+    try:
+        project = Business.objects.get(pk = id)
+        
+    except ObjectDoesNotExist:
+        raise Http404()    
+    
+    return render(request, "projects.html", {"project":project})
+  
 
