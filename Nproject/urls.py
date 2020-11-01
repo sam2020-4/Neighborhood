aa4dbfa86e9db7b906a46757038ce14de10fb40a
matata samuel <mattasamuel3@gmail.com>
2020-11-02 02:44:38 +0300

    inserted authentication urls

--- Nproject/urls.py
+++ Nproject/urls.py
@@ -19,8 +19,16 @@ from django.contrib.auth import views
 from registration.backends.simple.views import RegistrationView
 from neighborhood.forms import RegisterForm
 
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('neighborhood.urls')),
+    url(r'^accounts/register/$',
+        RegistrationView.as_view(
+            form_class=RegisterForm
+        ),
+        name='registration_register',
+    ),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+    url(r'^tinymce/', include('tinymce.urls')),    
 ]
