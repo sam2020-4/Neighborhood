496614e36c7852ef82cb9ff1e22f2fce1db243ff
matata samuel <mattasamuel3@gmail.com>
2020-11-02 01:45:04 +0300

    manupulated the data to be added to the system

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -15,5 +15,20 @@ def index(request):
 
     all_neighborhoods = Neighborhood.get_neighborhoods()
     
+    if 'neighborhood' in request.GET and request.GET["neighborhood"]:
+        neighborhoods = request.GET.get("neighborhood")
+        searched_neighborhood = Business.get_by_neighborhood(neighborhoods)
+        all_posts = Posts.get_by_neighborhood(neighborhoods)
+        message = f"{neighborhoods}"
+        all_neighborhoods = Neighborhood.get_neighborhoods()        
+        
+        return render(request, 'index.html', {"message":message,"location": searched_neighborhood,
+                                               "all_neighborhoods":all_neighborhoods, "all_posts":all_posts})
 
-    
+    else:
+        message = "No Neighborhood Found!"
+
+    return render(request, 'index.html', {"date": date, "all_neighborhoods":all_neighborhoods,})
+
+
+
