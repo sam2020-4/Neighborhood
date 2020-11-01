d29149eb837c513b9e1146817851cfc1ab11f235
matata samuel <mattasamuel3@gmail.com>
2020-11-02 02:36:41 +0300

    added a profile form

--- neighborhood/forms.py
+++ neighborhood/forms.py
@@ -14,3 +14,14 @@ class NewNeighborhoodForm(forms.ModelForm):
         model = Neighborhood
         exclude = ['Admin', 'pub_date', 'admin_profile']
     
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+        widgets = {
+          'bio': forms.Textarea(attrs={'rows':2, 'cols':10,}),
+        }
+
+
+
+
