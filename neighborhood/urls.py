81fd2e89b2e637842613b338478edfa39c7264f6
matata samuel <mattasamuel3@gmail.com>
2020-11-02 03:06:24 +0300

    added a app urls

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -5,6 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name= 'index'),
+    url(r'^search/', views.search_businesses, name='search_results'),
+    url(r'^business/(\d+)', views.get_business, name='business_results'),
+    url(r'^new/business$', views.new_business, name='new-business'),
+    url(r'^new/post$', views.new_post, name='new-post'),
+    url(r'^accounts/profile/$', views.user_profiles, name='profile'),
     
 ]
 
